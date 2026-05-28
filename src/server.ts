27f1df961461073531b3a5c75850124d901cef9f
federico tropiane <fedetrop23@gmail.com>
2026-05-27 23:55:23 -0300

    Agregamos Cors, helmet y morgan para el correcto manejo de peticiones http

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,11 @@
 import App from "./app";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
+
+App.use(morgan("dev"));
+App.use(helmet());
+App.use(cors());
 
 const PORT = 3000;
 
